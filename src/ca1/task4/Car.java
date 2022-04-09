87ba7ce1378e25937ea4ad1b42445e21b567f5e7
Patrick <patrickdepaz@Patricks-MacBook-Pro.local>
2022-04-09 22:00:33 +1000

    Car Implementation

--- src/ca1/task4/Car.java
+++ src/ca1/task4/Car.java
@@ -1,11 +1,48 @@
 package ca1.task4;
 
+	//Creating Car Class
 public class Car {
 	private String make;
 	private String model;
 	private String color;
 	
 	public Car(String make, String model, String color) {
-		
+		this.make = make;
+		this.model = model;
+		this.color = color;
+	}
+	// Implemented getter and setter to the field so we can access it outside the class
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public boolean equals(Car car2) {
+		// will return true if two car object is equal to the second car model and color
+		return getModel().equals(car2.getModel()) && getColor().equals(car2.getColor());
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s, %s, %s", getMake(), getModel(), getColor());
 	}
 }
--- src/ca1/task4/CarProcessing.java
+++ src/ca1/task4/CarProcessing.java
@@ -1,5 +1,8 @@
 package ca1.task4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarProcessing {
 	public static void main(String[] args) {
 		
@@ -17,14 +20,26 @@ public class CarProcessing {
 		};
 		
 		Car[] noDuplicates = cp.removeDuplicates(cars);
-		
+		System.out.println(noDuplicates.length);
 		for(Car c : noDuplicates ) {
 			System.out.println(c);
 		}
 	}
 	
 	public Car[] removeDuplicates(Car[] inputArray) {
-		return null;
+		List<Car> carList = new ArrayList<>();
+		for(int x = 0; x < inputArray.length; x++) {
+			boolean equal = false;
+			for(int j = 0; j < inputArray.length; j++) {
+				if(x != j && inputArray[x].equals(inputArray[j])) {
+					equal = true;
+					break;
+				}
+			}
+			if(!equal) carList.add(inputArray[x]);
+		}
+		// convert carList Arraylist object to Car[] array
+		return carList.toArray(Car[]::new);
 	}
 	
 }
